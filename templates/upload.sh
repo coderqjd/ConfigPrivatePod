1f2c98613eefbc1069a52e8a33507e3b61830042
qiangjindong <qiangjd@changtu.com>
2019-04-10 10:05:31 +0800

    1

--- templates/upload.sh
+++ templates/upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VersionString=`grep -E 's.version.*=' __ProjectName__.podspec`
-VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+# VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
 
 NewVersionNumber=$(($VersionNumber + 1))
 LineNumber=`grep -nE 's.version.*=' __ProjectName__.podspec | cut -d : -f1`
@@ -13,5 +13,5 @@ git add .
 git commit -am ${NewVersionNumber}
 git tag ${NewVersionNumber}
 git push origin master --tags
-pod repo push PrivatePods __ProjectName__.podspec --verbose --allow-warnings --use-libraries --use-modular-headers
+pod repo push qjd-specs __ProjectName__.podspec --verbose --allow-warnings --use-libraries --use-modular-headers
 
