11e4723564dd45b1ce8a617f0f677e9777ab5a82
qiangjindong <qiangjd@changtu.com>
2019-04-10 20:22:13 +0800

    修改发布命令

--- templates/upload.sh
+++ templates/upload.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 VersionString=`grep -E 's.version.*=' __ProjectName__.podspec`
-# VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+temp=${VersionString#*=}
+version=`tr -cd 0-9\. <<< "$temp"`
+#VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
 
-NewVersionNumber=$(($VersionNumber + 1))
-LineNumber=`grep -nE 's.version.*=' __ProjectName__.podspec | cut -d : -f1`
-sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" __ProjectName__.podspec
+#NewVersionNumber=$(($VersionNumber + 1))
+#LineNumber=`grep -nE 's.version.*=' CMCtop_Category.podspec | cut -d : -f1`
+#sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" CMCtop_Category.podspec
 
-echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+echo "current version is ${version}, new version is ${version}"
 
 git add .
-git commit -am ${NewVersionNumber}
-git tag ${NewVersionNumber}
+git commit -m ${version}
+git tag ${version}
 git push origin master --tags
 pod repo push qjd-specs __ProjectName__.podspec --verbose --allow-warnings --use-libraries --use-modular-headers
 
